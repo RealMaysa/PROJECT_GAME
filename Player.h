493775120f9a512340c3683636573e7cdbbdbffe
Pimpraethong Rotpotchanarat <86197283+RealMaysa@users.noreply.github.com>
2021-10-12 11:14:02 +0700

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,36 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+#include<SFML/Network.hpp>
+#include<SFML/Window.hpp>
+#include<SFML/Audio.hpp>
+#include<SFML/System.hpp>
+#include<iostream>
+class Player
+{
+private:
+  sf::Sprite sprite;
+  sf::Texture texture;
+  float movementSpeed;
+  float attackCooldown;
+  float attackCooldownMax;
+  //Private Functions
+  void initVariables();
+  void initTexture();
+  void initSprite();
+ 
+public:
+    Player();
+    virtual~Player();
+     //Accessor
+
+  const sf::Vector2f&getPos()const;
+
+    //Functions
+    void move(const float dirX,const float dirY);
+    const bool canAttack();//ถ้า attack แล้วจะreset
+    void updateAttack();
+    void update();
+    void render(sf::RenderTarget& target);
+};
+
+
