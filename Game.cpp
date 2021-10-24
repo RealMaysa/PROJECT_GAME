edcaec5c6916dcca94c6737bf5fcb56984eedc86
Pimpraethong Rotpotchanarat <86197283+RealMaysa@users.noreply.github.com>
2021-10-24 23:58:49 +0700

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -14,6 +14,35 @@ void Game::initTextures(){
  this->textures["BULLET"]= new sf::Texture();
  this->textures["BULLET"]->loadFromFile("texture/bullet.png");
 }
+
+void Game::initGUI(){
+
+ //Load Font
+if(!this->font.loadFromFile("Font/ZF#2ndPixelus.ttf")){
+
+
+   std::cout<< "ERROR"<<"\n";
+}
+
+//Init pointText
+this->pointText.setFont(this->font);
+this->pointText.setCharacterSize(100);
+this->pointText.setFillColor(sf::Color::White);
+this->pointText.setPosition(20.f,25.f);
+//GameOver
+this->gameOverText.setFont(this->font);
+this->gameOverText.setCharacterSize(300);
+this->gameOverText.setFillColor(sf::Color::White);
+this->gameOverText.setPosition(1600/2.f-550.f,1200/2.f+this->gameOverText.getLocalBounds().height/2.f-300.f);
+this->gameOverText.setString("GAME OVER");
+//init player GUI
+this->PlayerHpBar.setSize(sf::Vector2f(300.f,30.f));
+this->PlayerHpBar.setFillColor(sf::Color::Black);
+this->PlayerHpBar.setPosition(sf::Vector2f(20.f,20.f));
+this->PlayerHpBarBack=this->PlayerHpBar;//ให้ขนาดมันเท่ากัน
+this->PlayerHpBar.setFillColor(sf::Color::Green);
+
+}
 void Game::initPlayer(){
 
 	this->player=new Player();
@@ -30,14 +59,23 @@ this->worldbackgroundTex.loadFromFile("texture/Forest.jpg");
 this->worldbackground.setTexture(this->worldbackgroundTex);
 
 }
+void Game::initSystems(){
+
+this->point=0;
+
+
+}
 //ตัวที่สร้างและทำลาย
 Game::Game()
 {
 this->initWindow();
 this->initPlayer();
 this->initTextures();
+this->initGUI();
+this->initSystems();
 this->initWorld();
 this->initEnemies();
+
 }
 
 Game::~Game()
@@ -64,11 +102,11 @@ for(auto *i : this-> enemies){
 
 }
 
-
 //ฟังก์ชัน
 void Game::run(){
     while(this->window->isOpen())
-    {
+    {  this->pollEvents();
+      if(this->player->getHp()>0)
         this->update();
         this->render();
     }
@@ -97,13 +135,13 @@ while(this->window->pollEvent(this->ev)){ //ตราบใดที่มีก
 void Game::updateInput(){
  //Move Player	
 if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
- this->player->move(-1.f,0.f);
+ this->player->move(-2.f,0.f);
 if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
- this->player->move(1.f,0.f);
+ this->player->move(2.f,0.f);
 if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
- this->player->move(0.f,-1.f);
+ this->player->move(0.f,-2.f);
 if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
- this->player->move(0.f,1.f);
+ this->player->move(0.f,2.f);
 
 //&& this->player->canAttack()
 if(sf::Keyboard::isKeyPressed(sf::Keyboard::R)&&this->player->canAttack()){
@@ -113,45 +151,133 @@ this->bullets.push_back(new Bullet(this->textures["BULLET"],this->player->getPos
  
  }
 
+
+void Game::updateGUI(){
+std::stringstream ss;
+ss<<"Score: "<<this->point;
+this->pointText.setString(ss.str());
+//Update Player GUI
+
+float hpPercent=static_cast<float>(this->player->getHp())/this->player->getHpMax();
+this->PlayerHpBar.setSize(sf::Vector2f(300.f*hpPercent,this->PlayerHpBar.getSize().y));
+
+}
 void Game::updateBullets(){
 unsigned counter=0;
 for(auto*bullet : this->bullets)
 {
   bullet->update();
   //checkว่าชนขอบมั้ย
-  if(bullet->getBounds().width+bullet->getBounds().top<0.f){
+  if(bullet->getBounds().width+bullet->getBounds().left>=this->window->getSize().x){
 //Delete Bullet
 delete this->bullets.at(counter);
 this->bullets.erase(this->bullets.begin()+counter);
---counter;
   }
   ++counter;
 }
 
 }
+
+void Game::updateWorld(){
+
+
+
+}
+void Game::updateCollision(){
+  //เช็คชนกรอบ
+   //Left 
+  if(this->player->getBounds().left<0.f){
+
+    this->player->setPosition(0.f,this->player->getBounds().top);
+  }
+  //Right
+   if(this->player->getBounds().left+this->player->getBounds().width>=this->window->getSize().x){
+
+    this->player->setPosition(this->window->getSize().x-this->player->getBounds().width,this->player->getBounds().top);
+  }
+  //Top
+  if(this->player->getBounds().top<0.f){
+
+    this->player->setPosition(this->player->getBounds().left,0.f);
+  }
+  //Bottom
+  if(this->player->getBounds().top+this->player->getBounds().width>=this->window->getSize().y){
+
+    this->player->setPosition(this->player->getBounds().left,this->window->getSize().y-this->player->getBounds().height);
+  }
+}
 void Game::updateEnemies(){
-   this->spawnTimer+=0.5f;
- if(this->spawnTimer>=this->spawnTimerMax){
+  //spawning
+   this->spawnTimer+=0.2f;
+ if(this->spawnTimer>=this->spawnTimerMax){                //x             //y
       this->enemies.push_back(new Enemy(1200+rand()%1400,rand()%1000));//บริเวณนี้จะมีการspawnเกิดขึ้น
       this->spawnTimer=0.f;
 
- }
- for(auto *enemy : this->enemies)
+ }  
+ //update
+ unsigned counter=0;
+for(auto*enemy : this->enemies)
 {
-   enemy->update();
+  enemy->update();
+  //checkenemyว่าชนขอบมั้ย
+  if(enemy->getBounds().left<0.f){ 
+  //Delete enemy
+ delete this->enemies.at(counter);
+ this->enemies.erase(this->enemies.begin()+counter);
+--counter;
+  }
+  //Player Enemy Collision
+else if(enemy->getBounds().intersects(this->player->getBounds())){
+this->player->loseHp(this->enemies.at(counter)->getDamage());
+delete this->enemies.at(counter);
+ this->enemies.erase(this->enemies.begin()+counter);
+--counter;
 
+  }
+  ++counter;
+      
+ 
+}
+  }
+void Game::updateCombat(){
+for(int i=0;i<this->enemies.size();++i)
+{  bool enemy_deleted=false;
+for (size_t k= 0; k < this->bullets.size()&& enemy_deleted==false; k++)
+{
+  if(this->enemies[i]->getBounds().intersects(this->bullets[k]->getBounds())){
+
+    this->point+=this->enemies[i]->getPoints();
+     delete this->enemies[i];
+    this->enemies.erase(this->enemies.begin()+i);
+    delete this->bullets[k];
+    this->bullets.erase(this->bullets.begin()+k);
+    enemy_deleted=true;
+  }
 }
 
 
+
+}
 }
 void Game::update(){
 //update game logic การmove keyboard inputต่างๆ
-this->pollEvents();
 this->updateInput();
 this->player->update();
+this->updateCollision();
 this->updateBullets();
 this->updateEnemies();
+this->updateCombat();
+this->updateGUI();
+this->updateWorld();
 
+
+}
+void Game::renderGUI(){
+
+
+this->window->draw(this->pointText);
+this->window->draw(this->PlayerHpBarBack);
+this->window->draw(this->PlayerHpBar);
 
 }
 void Game::render(){
@@ -167,24 +293,28 @@ void Game::render(){
 
 this->window->clear();//clearก่อนเสมอ
 this->renderWorld();
-//Draw game objects//ให้ทุกอย่างrenderไปยังหน้าจอ
+//Draw game objects//ให้ทุกอย่างrenderไปยังหน้าจอ โดยจะเรนเดอร์เป็นลำดับจากบนไปล่างทับกันไปเรื่อยๆ ล่างสุดจะอยู่ด้านหน้าสุด
 this->player->render(*this->window);
 
-for(auto *bullet : this->bullets)
-{
-  bullet->render(this->window);
-
-}
 
 for(auto *enemy : this->enemies)
 {
 enemy->render(this->window);
 
 }
+for(auto *bullet : this->bullets)
+{
+  bullet->render(this->window);
+
+}
 
 
+this->renderGUI();
+//Game over Screen
+if(this->player->getHp()<=0){
+ this->window->draw(this->gameOverText);
 
-
+}
 this->window->display();
 
 
