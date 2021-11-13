a45d4bd461441ca3cf45d4a8c33ad70ecc19e59f
Pimpraethong Rotpotchanarat <86197283+RealMaysa@users.noreply.github.com>
2021-11-13 14:51:53 +0700

    Create GameState.h

---
+++ GameState.h
@@ -0,0 +1,19 @@
+/*#ifndef GAMESTATE_H
+#define GAMESTATE_H
+#include"State.h"
+class GameState:
+public State
+{
+private:
+  
+
+public:
+    GameState(sf::RenderWindow*window);
+    ~GameState();
+    //Function
+   void endState();
+    //void update();
+    void render(sf::RenderTarget*target=nullptr);
+};
+#endif
+*/
