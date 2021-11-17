b394e82c5a7f995787c3fcc51cc187e91ac19e02
Pimpraethong Rotpotchanarat <86197283+RealMaysa@users.noreply.github.com>
2021-11-18 02:07:34 +0700

    Update Game.h

--- Game.h
+++ Game.h
@@ -20,7 +20,8 @@
 #include"Flower_3.h"
 #include"Clover.h"
 #include"TextBox.h"
-
+#include"Bullet_2.h"
+#include"Enemy_3.h"
 //#include"State.h"
 //#include"GameState.h"
 //#include"mainMenu.h"
@@ -38,6 +39,8 @@ sf::Texture LDBbackgroundTex;
 sf::Sprite LDBbackground;
 sf::Texture TBbackgroundTex;
 sf::Sprite TBbackground;
+sf::Sprite TheEndBGsprite;
+sf::Texture TheEndBG;
 sf::Sprite buttonRsprite;
 sf::Texture buttonRtexture;
 sf::Sprite buttonExitsprite;
@@ -55,7 +58,8 @@ sf::VideoMode videoMode ;
 //std::stack<State*> states;//ในstateสร้างobjectไม่ได้เลยต้องสร้างได้แค่pointerชี้ไปยังมันและสร้างstackเพื่อเพิ่มstateใหม่ขึ้น
 
 //Resource  //คอนเทนเนอร์class
-std::map<std::string,sf:: Texture*>textures; //ในแต่ละstringจะกลายเป็นแต่ละtexture
+std::map<std::string,sf:: Texture*>textures;
+std::map<std::string,sf:: Texture*>textures_2; //ในแต่ละstringจะกลายเป็นแต่ละtexture
 /*ดังนั้นเมื่อมีการใส่บางอย่างเข้าไปในarray stringนี้ ในจุดเดียวกันก็จะมีtextureด้วย*/
 
 //World
@@ -76,7 +80,6 @@ unsigned point;
 unsigned high_score;
 //Bullets
 std::vector<Bullet*>bullets;
-
 //Player
 Player*player;
 
@@ -92,8 +95,10 @@ float spawnTimerMax_2;
 
 Enemy*enemy;
 Enemy_2* enemy_2;
+Enemy_3* enemy_3;
 std::vector<Enemy*> enemies;//เก็บค่าจาก class Enemy มาไว้ในvector
 std::vector<Enemy_2*> enemies_2;
+std::vector<Enemy_3*> enemies_3;
 //Item
   
 float dropTimer;
@@ -112,10 +117,18 @@ float countFlMax_3;
 float countFl_3;
 float countClMax;
 float countCl;
+float countE_2;
+float countEMAX_2;
+float countE_3;
+float countEMAX_3;
 float CooldownMax;
 float Cooldown;
 float Cooldown_2;
 float CooldownMax_2;
+float CooldowndamageMAx;
+float CooldowndamageMAx_2;
+float Cooldowndamage_2;
+float Cooldowndamage_3;
 Flower*flower;
 std::vector<Flower*> flowers;
 Flower_2*flower_2;
@@ -128,16 +141,19 @@ std::vector<Clover*> clovers;
 sf::String yourname;
 sf::Text nameplayer;
 sf::String name;
-/*std::vector<std::pair<int,std::string>> name_score;
 std::string nameJa;
+/*std::vector<std::pair<int,std::string>> name_score;
+
 int j=0,e,g;
 std::ifstream myfile;
 std::ofstream myfile2;*/
+//std::string nameJa;
 FILE*fp;
 char temp[218];
 int num=0;
 int savscore[6];
 std::string name_one[6];
+char*totalname;
 std::vector<std::pair<int,std::string>> user_score;
 //privarte function
    void initWindow();
@@ -148,6 +164,7 @@ std::vector<std::pair<int,std::string>> user_score;
    void initPlayer();
    void initEnemies();
    void initEnemies_2();
+   void initEnemies_3();
    void initFlower();
    void initFlower_2();
    void initFlower_3();
@@ -155,6 +172,7 @@ std::vector<std::pair<int,std::string>> user_score;
    void initWorld();
    void initSystems();
    void initMainMenuText();
+   void initTheEnd();
    void initLDB();
    void initButtonR();
    void initButtonEX();
@@ -178,17 +196,19 @@ void updateInput();
 void updateGUI();
 void updateEnemies();
 void updateEnemies_2();
+void updateEnemies_3();
 void updateCollision();
 void updateCombat();
 void updateBullets();
+void updateBullets_2();
 void updateFlower();
 void updateFlower_2();
 void updateFlower_3();
 void updateClover();
 void updateText();
 void updateText_2();
+
 const bool canText();
-const bool canText_2();
 void update(); 
 void renderGUI();
 void render();
