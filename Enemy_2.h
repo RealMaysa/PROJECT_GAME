31f6ba9e9c39404502401e8b22d11f2f91413ebb
Pimpraethong Rotpotchanarat <86197283+RealMaysa@users.noreply.github.com>
2021-11-18 02:07:11 +0700

    Update Enemy_2.h

--- Enemy_2.h
+++ Enemy_2.h
@@ -2,6 +2,7 @@
 #define ENEMY_2_H
 #include<SFML/Graphics.hpp>
 #include<iostream>
+
 //มีเรื่องรูปร่างของศัตรู HP และ Damageของแต่ละตัว
 class Enemy_2
 {
@@ -9,12 +10,13 @@ private:
   sf::Sprite enemyWsprite;
   sf::Texture enemyWtexture;
   float movementSpeed;
-    int type;
-    int speed;
     int hp;
     int hpMax;
+    float speed;
     int damage;
     int points;
+    float attackCooldown;
+    float attackCooldownMax;
     void initTexture();
     void initEnemySprite();
     void initVariables();
@@ -25,6 +27,13 @@ public:
     const sf::FloatRect getBounds() const;
     const int& getPoints() const;
     const int& getDamage() const;
+    void loseHp(const int value);
+    void setHp(const int hp);
+    const int&getHp() const;
+    const int&getHpMax() const;
+    const bool canAttack();
+    void updateAttack();
+    inline const bool isDead()const{return this->hp==0;}
     void update();
     void render(sf::RenderTarget*target);
 };
