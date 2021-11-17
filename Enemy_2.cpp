eeb8c0c267710caf6651c38e3df86e0f93f8d4bf
Pimpraethong Rotpotchanarat <86197283+RealMaysa@users.noreply.github.com>
2021-11-18 02:07:09 +0700

    Update Enemy_2.cpp

--- Enemy_2.cpp
+++ Enemy_2.cpp
@@ -12,14 +12,14 @@ void Enemy_2::initEnemySprite(){
 // Set texture to the sprite
 this->enemyWsprite.setTexture(this->enemyWtexture);
 //ปรับขนาดของSprite
-this->enemyWsprite.scale(3.f,3.f);
+this->enemyWsprite.scale(2.f,2.f);
 
 }
 void Enemy_2::initVariables(){
-this->speed=-1.7f;
-this->hp=0;
-this->hpMax=10;
-this->points=100;
+this->speed=-1.4f;
+this->hpMax=50;
+this->hp=this->hpMax;
+this->points=300;
 this->damage=10;
 
 
@@ -39,11 +39,29 @@ Enemy_2::~Enemy_2()
 }
 
 
-
+void Enemy_2::loseHp(int value){
+    this->hp-=value;//set ทุกอย่างที่เข้ามาในนี้ให้ลบHp
+   if(this->hp<0){
+    this->hp=0;
+        }
+}
 const sf::FloatRect Enemy_2::getBounds() const
 {
 
     return this->enemyWsprite.getGlobalBounds();
+}
+const int &Enemy_2::getHp() const{
+    return this->hp;
+}
+const int &Enemy_2::getHpMax() const{
+
+    return this->hpMax;
+}
+void Enemy_2::setHp(int hp){
+
+
+this->hp=hp;
+
 }
 const int &Enemy_2::getPoints() const{
 
@@ -54,10 +72,24 @@ const int &Enemy_2::getDamage() const{
 return this->damage;
 
 }
+const bool Enemy_2::canAttack(){
+
+if(this->attackCooldown>=this->attackCooldownMax){
+    this->attackCooldown=0.f;//ให้มันเริ่มนับใหม่
+    return true;
+}
+
+    return false;
+}
+void Enemy_2::updateAttack(){
+if(this->attackCooldown < this->attackCooldownMax){
+    this->attackCooldown+=0.5f;//เพิ่มคูลดาวน์ทีละ0.5
+    }
+
+}
 void Enemy_2::update(){
 
 this->enemyWsprite.move(this->speed,0.f);
-
 }
 
 void Enemy_2::render(sf::RenderTarget *target){
